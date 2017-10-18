13196a6021decfddccc2dd29244ad2024692a43c
CHILLI\Abraham <abraham.neethling@chillisoft.co.za>
2017-10-18 08:40:13 +0200

    Moo Cows

---
+++ DAL/TableContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using PersistantDictionary.Models;
+
+
+namespace PersistantDictionary.DAL
+{
+    public class TableContext : DbContext
+    {
+        public TableContext() : base("TableContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
